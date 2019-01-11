106ce1228a1beeaec3835463ee3e78696c73e795
niranj1234 <46585465+niranj1234@users.noreply.github.com>
2019-01-11 13:47:07 +0530

    Update bb1.py

--- bb1.py
+++ bb1.py
@@ -1,7 +1,7 @@
 a=int(input())
 if(a>0):
-    print("positive number")
+    print("Positive")
 elif(a==0):
-    print("zero number")
+    print("Zero")
 else:
-    print("negative")
+    print("Negative")
